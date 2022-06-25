3af82f41af3458c67915dea2c2f7880e77541b40
Sh-OV <shchegolga@yandex.ru>
2022-06-25 21:14:42 +0500

    Task_001 Мое решение

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿int a = new Random().Next(-100, 101);
+Console.WriteLine("Чило, квадрат которого надо найти: " + a);
+int kvadrat = a * a;
+Console.WriteLine($"Квадрат числа равен = {kvadrat}");
