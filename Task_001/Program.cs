c95ce5a6392e9cb5fab906ece6286449656c9080
Sh-OV <shchegolga@yandex.ru>
2022-06-26 11:09:15 +0500

    Task_001 решение в классе

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,4 +1,7 @@
-﻿int a = new Random().Next(-100, 101);
-Console.WriteLine("Чило, квадрат которого надо найти: " + a);
+﻿Console.Clear();
+//int a = new Random().Next(-100, 101);
+Console.Write("Введите любое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Чило, квадрат которого надо найти: " + a);
 int kvadrat = a * a;
-Console.WriteLine($"Квадрат числа равен = {kvadrat}");
+Console.WriteLine($"Квадрат числа {a} равен = {kvadrat}");
