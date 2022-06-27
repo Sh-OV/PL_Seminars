68b9ade94b280a84cb3a475bb1011067d1c76a7b
Sh-OV <shchegolga@yandex.ru>
2022-06-27 10:42:00 +0500

    Task_02 решено с помощью оператора if / else

---
+++ Task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает,
+// какое число больше, а какое меньше
+Console.Clear();
+Console.Write("Введите любое натуральное число: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще одно любое натуральное число: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+if (first_num >= second_num)
+{
+    Console.WriteLine($"Максимальное число из двух введенных чисел: max = {first_num}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число из двух введенных чисел: max = {second_num}");
+}
