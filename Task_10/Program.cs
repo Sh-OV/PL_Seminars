9f4702a74a98a44482fba4e4cc8e51c3446fd1a1
Sh-OV <shchegolga@yandex.ru>
2022-06-28 00:16:57 +0500

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.*/
+ Console.Write("Введите трехзначное число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ if(number < 100 || number >= 1000) 
+ {
+    Console.Write("Вы ввели неверное число. Пожалуйста введите трехзначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+ }
+ Console.WriteLine($"Вторая цифра числа {number} будет равна {number / 10 % 10}");
