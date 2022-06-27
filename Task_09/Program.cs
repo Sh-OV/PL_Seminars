42b8e88a30bafb1f65570e960fdb1f176530b3f0
Sh-OV <shchegolga@yandex.ru>
2022-06-27 18:26:51 +0500

    Task_09 добавила решение преподавателя с помсощью тернарного оператора (альтернатива оператору if

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] 
 // и показывает наибольшую цифру числа
 Console.Clear();
+Console.WriteLine("Решение № 1");
 void BigNum (int random)
 {
     int number1 = random / 10;
@@ -10,3 +11,14 @@ void BigNum (int random)
 }
 int rand = new Random().Next(10, 100);
 BigNum(rand);
+Console.WriteLine();
+Console.WriteLine("Решение № 2");
+
+int MaxDigit(int randNum)
+{
+    int num1 = randNum / 10;
+    int num2 = randNum % 10;
+    int Max = num1 > num2 ? num1 : num2;
+    return Max;
+}
+Console.WriteLine($"Наибольшая цифра случайного числа {rand}: {MaxDigit(rand)}");
