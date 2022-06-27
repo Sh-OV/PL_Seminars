b420df801d23cbba5e5778e5da9b8dcc2da1ee91
Sh-OV <shchegolga@yandex.ru>
2022-06-27 09:41:53 +0500

    Task_07 Решение реализовано через выделение остатка

---
+++ Task_07/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число
+// и на выходе показывает последнюю цифру этого числа
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1000)
+{
+    Console.Write("Вы ввели неверное число, пожалуйста введите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+int rem = num % 100;
+rem %= 10;
+Console.WriteLine($"Последняя цифра числа {num} будет {rem}");
