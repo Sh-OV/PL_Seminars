5ffc158b39cd70584a9eb39bf5f2414d9554e850
Sh-OV <shchegolga@yandex.ru>
2022-06-28 02:19:52 +0500

    Task_13 решение методом с циклами... возможно можно проще?

---
+++ Task_13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+Console.Write("Введите любое целое число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ 
+ int ThirdDigit(int num)
+ {
+    int i = 1;
+    int digit = num;
+
+    while (num / 10 > 0)
+    {
+        num /= 10;
+        i++;
+    }
+    int count = i;
+    i = count-3;
+    if(i > 0)
+    {
+        count = 1;
+       while (i > 0)
+        {
+            count *= 10;
+            i--;
+        }
+    i = count;
+     num = digit / i % 10;
+    }
+    else num = -1;
+    return num;
+ }
+ string result = ThirdDigit(number) > 0 ? 
+        $"Третья цифра заданного числа равна {ThirdDigit(number)}" : 
+        "Третьей цифры НЕТ";
+ Console.WriteLine(result);
