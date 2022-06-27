aff5fddcc1a92f6e9ba0dc7b2ab5d9d3733aeb15
Sh-OV <shchegolga@yandex.ru>
2022-06-27 11:37:05 +0500

    Task_08 добавила второе решение через цикл for

--- Task_08/Program.cs
+++ Task_08/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Напишите программу, котоорая на вход получает число N, а на выходе показывает все четные числа от 1 до N
 Console.Clear();
+Console.WriteLine("РЕШЕНИЕ № 1");
 Console.Write("Введите любое натуральное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = 2;
@@ -7,4 +8,13 @@ while (count <= num)
 {
     Console.Write($"{count} ");
     count += 2;
+}
+Console.WriteLine();
+
+Console.WriteLine("РЕШЕНИЕ № 2");
+Console.Write("Введите любое натуральное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= num; i+=2)
+{
+    Console.Write($"{i} ");
 }
