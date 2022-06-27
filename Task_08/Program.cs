b728ea9b5af8715a9a9d095af1875156cc0aaac1
Sh-OV <shchegolga@yandex.ru>
2022-06-27 11:25:57 +0500

    Task_08 решение через цикл while

---
+++ Task_08/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, котоорая на вход получает число N, а на выходе показывает все четные числа от 1 до N
+Console.Clear();
+Console.Write("Введите любое натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count += 2;
+}
