1f2c86fd9b744a97d6efc3e1e71ec43c0b62e1d3
Sh-OV <shchegolga@yandex.ru>
2022-06-28 10:18:44 +0500

    Task_15 решение с использованием массива

---
+++ Task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите день недели числом от 1 до 7: ");
+int numday = Convert.ToInt32(Console.ReadLine());
+if (numday > 7 || numday < 1)
+{
+    Console.Write("Такого дня недели нет! Введите, пожалуйста, день недели числом от 1 до 7: ");
+    numday = Convert.ToInt32(Console.ReadLine());
+}
+int i = 0;
+string [] arrday = {"NULL","Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+if (numday == 6 || numday == 7)
+{
+    Console.WriteLine($"Введенный день недели {numday} это {arrday[numday]}, сегодня ВЫХОДНОЙ!!!");
+}
+else Console.WriteLine($"Введенный день недели {numday} это {arrday[numday]}, обычный день...");
