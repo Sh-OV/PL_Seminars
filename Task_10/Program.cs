45eb54f72f87054cdce3e6d1f6033e8782ab4880
Sh-OV <shchegolga@yandex.ru>
2022-06-28 17:52:31 +0500

    Task_10 откорректировала по замечаниям преподавателя

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -2,7 +2,7 @@
  и на выходе показывает вторую цифру этого числа.*/
  Console.Write("Введите трехзначное число: ");
  int number = Convert.ToInt32(Console.ReadLine());
- if(number < 100 || number >= 1000) 
+ if(number < 100 || number > 1000) 
  {
     Console.Write("Вы ввели неверное число. Пожалуйста введите трехзначное число: ");
     number = Convert.ToInt32(Console.ReadLine());
