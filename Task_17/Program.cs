ce9dfe2b5f924bbaa8221a6cd942d2694d17326c
Sh-OV <shchegolga@yandex.ru>
2022-07-02 00:19:07 +0500

    Task_17 не получается пока решить через switch

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача № 17. Напишите программу, которая принимает на вход две точки (X; Y), причем X != 0 и Y != 0,
 // и выдает номер четверти плоскости в которой находится эта точка (нумерация плоскостей против часовой
 // стрелки, с 1-4, начиная с положительной плоскости для X и Y)
+Console.WriteLine("Решение № 1");
 Console.Write("Введите координату по оси Х: ");
 int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату по оси Y: ");
@@ -18,3 +19,27 @@ string Quarter(int xc, int yc)
 
 string result = Quarter(x, y);
 Console.Write(result);
+
+Console.WriteLine("Решение № 2");
+Console.Write("Введите координату по оси Х: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+switch (x1)
+{
+    case x1 > 0 :
+    switch (y1)
+    {
+        case y1 > 0 : "Точка находится в первой четверти плоскости"; break;
+        case y1 < 0 : "Точка находится в четвертой четверти плоскости"; break;
+    }
+    case x1 < 0 :
+    switch (y1)
+    {
+        case y1 > 0 : "Точка находится во второй четверти плоскости"; break;
+        case y1 < 0 : "Точка находится в третьей четверти плоскости"; break;
+    }
+    default: "Введены некорректные координаты"; break;
+}
+
