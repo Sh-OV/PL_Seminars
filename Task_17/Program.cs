2e6d529bd414f62777f88580d3ae8c4b641d255a
Sh-OV <shchegolga@yandex.ru>
2022-07-02 23:07:52 +0500

    Task_17 закоментировала пока решение №2 через switch - не получается

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -13,33 +13,33 @@ string Quarter(int xc, int yc)
     if (xc > 0 && yc > 0) return "Точка находится в первой четверти плоскости";
     if (xc < 0 && yc > 0) return "Точка находится во второй четверти плоскости";
     if (xc < 0 && yc < 0) return "Точка находится в третьей четверти плоскости";
-    if (xc < 0 && yc < 0) return "Точка находится в четвертой четверти плоскости";
+    if (xc > 0 && yc < 0) return "Точка находится в четвертой четверти плоскости";
     else return "Введены некорректные координаты";
 }
 
 string result = Quarter(x, y);
 Console.Write(result);
 
-Console.WriteLine("Решение № 2");
-Console.Write("Введите координату по оси Х: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси Y: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Решение № 2");
+// Console.Write("Введите координату по оси Х: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси Y: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
 
-switch (x1)
-{
-    case x1 > 0 :
-    switch (y1)
-    {
-        case y1 > 0 : "Точка находится в первой четверти плоскости"; break;
-        case y1 < 0 : "Точка находится в четвертой четверти плоскости"; break;
-    }
-    case x1 < 0 :
-    switch (y1)
-    {
-        case y1 > 0 : "Точка находится во второй четверти плоскости"; break;
-        case y1 < 0 : "Точка находится в третьей четверти плоскости"; break;
-    }
-    default: "Введены некорректные координаты"; break;
-}
+// switch (x1)
+// {
+//     case x1 > 0 :
+//     switch (y1)
+//     {
+//         case y1 > 0 : "Точка находится в первой четверти плоскости"; break;
+//         case y1 < 0 : "Точка находится в четвертой четверти плоскости"; break;
+//     }
+//     case x1 < 0 :
+//     switch (y1)
+//     {
+//         case y1 > 0 : "Точка находится во второй четверти плоскости"; break;
+//         case y1 < 0 : "Точка находится в третьей четверти плоскости"; break;
+//     }
+//     default: "Введены некорректные координаты"; break;
+// }
 
