6255febb3c59ba00dc48d0d118e2979b58e7eee6
Sh-OV <shchegolga@yandex.ru>
2022-07-03 02:58:03 +0500

    Task_23 решение через цикл while

---
+++ Task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача № 23. Напишите программу, которая принимает на вход число N 
+// и выдает таблицу кубов чисел от 1 до N
+Console.Write("Введите любое натуральное число до 100: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count < number)
+{
+    Console.Write($"{Math.Pow(count,3)}, ");
+    count++;
+}
+Console.WriteLine(Math.Pow(number,3));
