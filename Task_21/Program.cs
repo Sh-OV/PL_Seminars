5b5ce0602805a8aab4597be7aa48db9bde073b13
Sh-OV <shchegolga@yandex.ru>
2022-07-03 00:43:50 +0500

    Task_21 решила с применением Math.Pow and Math.Round

---
+++ Task_21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача № 21. Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+Console.Write("Введите координату x первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance (int cx1, int cy1, int cx2, int cy2)
+{
+    int x = cx1 - cx2;
+    int y = cy1 - cy2;
+    double dist = Math.Pow((x*x + y*y),0.5);
+    return Math.Round(dist,2);
+}
+double result =  Distance (x1, y1, x2, y2);
+Console.Write(result);
