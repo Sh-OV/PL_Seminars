2819e96d48ed5a2eb4ceea45d7eb569ed7387e83
Sh-OV <shchegolga@yandex.ru>
2022-07-03 03:12:50 +0500

    Task_23 добавила текст при выводе ответа

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -3,6 +3,7 @@
 Console.Write("Введите любое натуральное число до 100: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
+ Console.Write($"Таблица кубов чисел от 1 до {number}:    ");
 while (count < number)
 {
     Console.Write($"{Math.Pow(count,3)}, ");
