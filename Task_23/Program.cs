c9b22bd18ce52660e449046e6b516e03404baee9
Sh-OV <shchegolga@yandex.ru>
2022-07-03 15:36:28 +0500

    Task_23 исправила решение по замечаниям преподавателя (оформила вывод в виде таблицы значений

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задача № 23. Напишите программу, которая принимает на вход число N 
 // и выдает таблицу кубов чисел от 1 до N
+Console.Clear();
 Console.Write("Введите любое натуральное число до 100: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
- Console.Write($"Таблица кубов чисел от 1 до {number}:    ");
-while (count < number)
+ Console.WriteLine($"Таблица кубов чисел от 1 до {number}:    ");
+while (count <= number)
 {
-    Console.Write($"{Math.Pow(count,3)}, ");
+    Console.WriteLine($"{count}^3 => {Math.Pow(count,3)}");
     count++;
 }
-Console.WriteLine(Math.Pow(number,3));
