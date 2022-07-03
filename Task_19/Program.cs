ebc389eb1530b89b539b8693bc4d2c13566d51b5
Sh-OV <shchegolga@yandex.ru>
2022-07-03 15:53:56 +0500

    Task_19 исправила по замечаниям преподавателя - реализовала проверку правильности ввода числа через цмкл while

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Задаче № 19. Напишите программу, которая принимает на вход пятизначное число и проверяет
 // является ли оно палиндромом
-Console.WriteLine("Решение №1 по ТЗ задачи: ");
-Console.Write("ВВедите пятизначное число: ");
+// Console.WriteLine("Решение №1 по ТЗ задачи: ");
+Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number < 10000 || number > 99999)
-{
-    Console.Write("Вы ввели некорректное число, введите пятизначное число: ");
-    number = Convert.ToInt32(Console.ReadLine());
-} 
+
+    while (number < 10000 || number > 99999)
+    {
+        Console.Write("Вы ввели некорректное число, введите пятизначное число: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    } 
 
 string Palindrome(int num)
 {
@@ -22,5 +23,6 @@ string Palindrome(int num)
     else { answer = $"Число {num} не являеется полиндромом, это обычное число";}
     return answer;
 }
-string result = Palindrome(number);
-Console.WriteLine(result);
+
+    string result = Palindrome(number);
+    Console.WriteLine(result);
