0ff95356e918f3df52bdcb6d2b90274ed6ac3593
Sh-OV <shchegolga@yandex.ru>
2022-07-05 23:13:54 +0500

    Task_27 решение методом и циклом

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите любое целое число от -1 000 000 000 до 1 000 000 000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum (int num)
+{
+    int sum = 0;
+    while (0 < Math.Abs(num))
+    {
+        sum += num % 10;
+        num = num / 10;
+        
+        Console.WriteLine($"num = {num};  sum = {sum}");
+    }
+    return sum;
+}
+int result = Sum (number);
+Console.WriteLine($"Сумма цифр в числе {number} = {result}");
