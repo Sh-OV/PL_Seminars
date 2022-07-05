bf55eb8b22ddf0077fe0740ddbf0bdd5d1946259
Sh-OV <shchegolga@yandex.ru>
2022-07-05 17:26:21 +0500

    Task_26 мое решение

---
+++ Task_26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача № 26. Напишите программу, которая принимает  на вход число и выдает количекство цифр в числе.
+Console.Write("Введите любое целое число до 1 000 000 000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Quantity (int num)
+{
+    int count = 0;
+    while (0 < num)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+int result = Quantity (number);
+Console.WriteLine($"Количество цифр в числе {number}: {result}");
