45648f5a9c88ac0beceb32628289bccf483639ad
Sh-OV <shchegolga@yandex.ru>
2022-07-13 00:35:15 +0500

    Task_41 добавила метод правильного окончания в зависимости от цифры

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -5,25 +5,29 @@ Console.Clear();
 int[] UserArray()
 {
     // Можно так:
-    // Console.Write("Введите размер массива, который Вы будете вводить: ");
-    // int lenght = Convert.ToInt32(Console.ReadLine());
-    // Console.WriteLine("Введите указанное Вами количество любых чисел: ");
-    // int[] arrNum = new int[lenght];
-    // через цикл for
-
+    Console.Write("Введите размер массива, который Вы будете вводить: ");
+    int len = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите указанное Вами количество любых чисел: ");
+    int[] arrNum = new int[len];
+    int num = 0;
     // Можно так:
-    Console.WriteLine("Введите несколько чисел (не более 100) любого значения, ");
-    Console.WriteLine("чтобы завершить ввод чисел напишите четыре единицы (1111): ");
-    int[] arrNum = new int[100];
-    int num = Convert.ToInt32(Console.ReadLine());
-    int i = 0;
-    while (num != 1111)
+    // Console.WriteLine("Введите несколько чисел (не более 100) любого значения, ");
+    // Console.WriteLine("после, чтобы завершить ввод чисел, напишите четыре единицы (1111): ");
+    // int[] arrNum = new int[100];
+    // int num = Convert.ToInt32(Console.ReadLine());
+    // int i = 0;
+    // while (num != 1111)
+    // {
+    //     arrNum[i] = num;
+    //     num = Convert.ToInt32(Console.ReadLine());   
+    //     i++;   
+    // }
+    for (int i = 0; i < len; i++)
     {
-        arrNum[i] = num;
-        num = Convert.ToInt32(Console.ReadLine());   
-        i++;   
+         num = Convert.ToInt32(Console.ReadLine());
+         arrNum[i] = num;
     }
-    return arrNum;
+     return arrNum;
 }
 int[] userArr = UserArray();
 
@@ -48,6 +52,21 @@ void PrintArr (int[] res)
         else Console.WriteLine($"{res[i]}]");
     }
 }
+
+string EndWord(int digit)
+{
+    string end = String.Empty;
+    
+    if (digit % 10 == 1 && digit / 10 <= 10) end = "положительное значение";
+    else if ((digit % 10 == 2 || digit % 10 == 3 || digit % 10 == 4) && digit / 10 <= 10) end = "положительных значения";
+    else if ((digit / 10 >= 10 && digit / 10 <= 20) || digit % 10 == 0 || 
+              digit % 10 == 5 || digit % 10 == 6 || digit % 10 == 7 ||
+              digit % 10 == 8 || digit % 10 == 9) end = "положительных значений";
+
+    return end;  
+}
+string res = EndWord(num);
+
 Console.WriteLine("В созданном Вами одномерном массиве:");
 PrintArr(userArr);
-Console.WriteLine($"имеется  {num}   положительных значений");
+Console.WriteLine($"имеется  {num}  {res}");
