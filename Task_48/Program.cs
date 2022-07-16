c370f3cee02696dfded39edce9ffdc1bdbf86d40
Sh-OV <shchegolga@yandex.ru>
2022-07-16 12:31:47 +0500

    Task_48 мое решение

---
+++ Task_48/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача № 48. Задайте двумерный массив размера m*n,
+// каждый элемент в массиве находится по формуле Amn = m + n.
+// Выведите полученный массив на экран.
+Console.Clear();
+Console.Write("Введите количество рядов двумерного массива: ");
+int oneLevelArray = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива: ");
+int twoLevelArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] TwoMatrix (int row, int col)
+{
+    int[,] matrix  = new int [row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    }
+    return matrix;
+}
+int[,] twoMatrix = TwoMatrix (oneLevelArray, twoLevelArray);
+
+void PrintTwoMatrix(int[,] matrix)
+{
+    string digit = String.Empty;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if     (matrix[i,j] >= 0 && matrix[i,j] < 10) digit = $"      {matrix[i,j]}";
+        else if((matrix[i,j] > 10 && matrix[i,j] < 100) || 
+                (matrix[i,j] > -10 && matrix[i,j] < 0)) digit = $"     {matrix[i,j]}";
+        else if((matrix[i,j] > 100 && matrix[i,j] < 1000) ||
+                (matrix[i,j] > -100 && matrix[i,j] <= -10)) digit = $"    {matrix[i,j]}";
+        else if((matrix[i,j] > 1000 && matrix[i,j] < 10000) ||
+                (matrix[i,j] > -1000 && matrix[i,j] <= -100)) digit = $"   {matrix[i,j]}";
+        else if((matrix[i,j] > 10000 && matrix[i,j] < 1000000) || 
+                (matrix[i,j] > -10000 && matrix[i,j] < -1000 )) digit = $"  {matrix[i,j]}";
+        else if((matrix[i,j] > 100000 && matrix[i,j] < 10000000) || 
+                (matrix[i,j] > -100000 && matrix[i,j] < -10000 )) digit = $" {matrix[i,j]}";
+  
+        if (j == 0) Console.Write("{ ");
+        if (j < matrix.GetLength(1)-1)  Console.Write($"{digit} | ");
+        if (j == matrix.GetLength(1)-1) Console.Write($"{digit} }}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintTwoMatrix(twoMatrix);
+Console.WriteLine();
