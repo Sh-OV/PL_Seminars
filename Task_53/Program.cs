fd219882c164c6af3a38259bb8dc53ac3fbcfaa4
Sh-OV <shchegolga@yandex.ru>
2022-07-20 23:47:46 +0500

    Task_53 мое решение

---
+++ Task_53/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача № 53. Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+Console.Clear();
+int rows = 5;
+int columns = 5;
+int minnum = -100;
+int maxnum = 100;
+
+int[,] RandomNewMatrix (int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;   
+}
+int[,] randMatrix = RandomNewMatrix (rows, columns, minnum, maxnum);
+
+void PrintTwoMatrix(int[,] matrix)
+{
+    string digit = String.Empty;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if(matrix[i,j] >= 0 && matrix[i,j] < 10) digit = $"      {matrix[i,j]}";
+        else if((matrix[i,j] > 10 && matrix[i,j] < 100) || 
+                (matrix[i,j] > -10 && matrix[i,j] < 0)) digit = $"     {matrix[i,j]}";
+        else if((matrix[i,j] > 100 && matrix[i,j] < 1000) ||
+                (matrix[i,j] > -100 && matrix[i,j] <= -10)) digit = $"    {matrix[i,j]}";
+        else if((matrix[i,j] > 1000 && matrix[i,j] < 10000) ||
+                (matrix[i,j] > -1000 && matrix[i,j] <= -100)) digit = $"   {matrix[i,j]}";
+        else if((matrix[i,j] > 10000 && matrix[i,j] < 1000000) || 
+                (matrix[i,j] > -10000 && matrix[i,j] < -1000 )) digit = $"  {matrix[i,j]}";
+        else if((matrix[i,j] > 100000 && matrix[i,j] < 10000000) || 
+                (matrix[i,j] > -100000 && matrix[i,j] < -10000 )) digit = $" {matrix[i,j]}";
+ 
+        if (j == 0) Console.Write("{ ");
+        if (j < matrix.GetLength(1)-1)  Console.Write($"{digit} | ");
+        if (j == matrix.GetLength(1)-1) Console.Write($"{digit} }}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintTwoMatrix(randMatrix);
+Console.WriteLine();
+
+
+int[,] ReplacingRowsMatrix (int[,] matrix)
+{
+    int support = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                support = matrix[i,j];
+                matrix[i,j] = matrix[matrix.GetLength(1)-1,j];
+                matrix[matrix.GetLength(1)-1,j] = support;
+            }
+        }
+    }
+    return matrix;
+}
+int[,] replasingMatrix = ReplacingRowsMatrix (randMatrix);
+
+
+PrintTwoMatrix(replasingMatrix);
+Console.WriteLine();
