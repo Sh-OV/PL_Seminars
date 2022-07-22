8715c039d5688f318bafefcb41fccece89495961
Sh-OV <shchegolga@yandex.ru>
2022-07-22 15:28:08 +0500

    Task_54 мое решение

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -1,17 +1,87 @@
 ﻿/* Задача 54:   Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
                 элементы каждой строки двумерного массива.
 Например, задан массив:
-
 1 4 7 2
-
 5 9 2 3
-
 8 4 2 4
-
 В итоге получается вот такой массив:
-
 7 4 2 1
-
 9 5 3 2
-
 8 4 4 2    */
+
+Console.Clear();
+Console.Write("Введите количество рядов двумерного массива: ");
+int oneLevelArray = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива: ");
+int twoLevelArray = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение массива: ");
+int minElements = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение массива: ");
+int maxElements = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] RandTwoMatrix(int row, int col, int min, int max) // метод создания двумерного массива рандомно
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+int[,] randMatrix = RandTwoMatrix(oneLevelArray, twoLevelArray, minElements, maxElements);
+
+void PrintTwoMatrix(int[,] matrix)      // метод вывода двумерного массива на печать
+{
+    string digit = String.Empty;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] >= 0 && matrix[i, j] < 10) digit = $"      {matrix[i, j]}";
+            else if ((matrix[i, j] > 10 && matrix[i, j] < 100) ||
+                    (matrix[i, j] > -10 && matrix[i, j] < 0)) digit = $"     {matrix[i, j]}";
+            else if ((matrix[i, j] > 100 && matrix[i, j] < 1000) ||
+                    (matrix[i, j] > -100 && matrix[i, j] <= -10)) digit = $"    {matrix[i, j]}";
+            else if ((matrix[i, j] > 1000 && matrix[i, j] < 10000) ||
+                    (matrix[i, j] > -1000 && matrix[i, j] <= -100)) digit = $"   {matrix[i, j]}";
+            else if ((matrix[i, j] > 10000 && matrix[i, j] < 1000000) ||
+                    (matrix[i, j] > -10000 && matrix[i, j] < -1000)) digit = $"  {matrix[i, j]}";
+            else if ((matrix[i, j] > 100000 && matrix[i, j] < 10000000) ||
+                    (matrix[i, j] > -100000 && matrix[i, j] < -10000)) digit = $" {matrix[i, j]}";
+
+            if (j == 0) Console.Write("[");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{digit} ");
+            if (j == matrix.GetLength(1) - 1) Console.Write($"{digit} ]");
+        }
+        Console.WriteLine();
+    }
+}
+PrintTwoMatrix(randMatrix);
+Console.WriteLine();
+
+int[,] SortingRowMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1)-1; j++)
+        {
+            int maxPosition = j;  
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if(matrix[i, k] > matrix[i, maxPosition]) maxPosition = k;
+            }
+            int property = matrix[i, j];
+            matrix[i, j] = matrix[i, maxPosition];
+            matrix[i, maxPosition] = property;
+        }
+    }
+    return matrix;
+}
+int[,] sortMatrix = SortingRowMatrix (randMatrix);
+PrintTwoMatrix(sortMatrix);
+Console.WriteLine();
