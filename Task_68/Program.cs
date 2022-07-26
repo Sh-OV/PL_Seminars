7d03cac2ca5b2e753ced3598a0d87b327853a92a
Sh-OV <shchegolga@yandex.ru>
2022-07-26 15:33:11 +0500

    Task_69 мое решение

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -2,3 +2,36 @@
                 Даны два неотрицательных числа m и n.
 
 m = 3, n = 2 -> A(m,n) = 29     */
+Console.Clear();
+Console.WriteLine("Для вычисления функции Аккермана введите два неотрицательных числа m и n ");
+Console.Write("Введите неотрицательное число m (от 0 до 3): ");
+int firstNonNegativeNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число n (от 0 до 10): ");
+int secondNonNegativeNumber = Convert.ToInt32(Console.ReadLine());
+while (firstNonNegativeNumber < 0 || firstNonNegativeNumber > 3)
+{
+    Console.WriteLine("Вы ввели неверные данные. Число m НЕ должно быть отрицательным и НЕ должно быть больше 3!");
+    Console.Write("Введите неотрицательное число m (от 0 до 3): ");
+    firstNonNegativeNumber = Convert.ToInt32(Console.ReadLine());
+}
+while (secondNonNegativeNumber < 0 || secondNonNegativeNumber > 11)
+{
+    Console.WriteLine("Вы ввели неверные данные. Число m НЕ должно быть отрицательным и НЕ должно быть больше 10!");
+    Console.Write("Введите неотрицательное число n (от 0 до 10): ");
+    secondNonNegativeNumber = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+ulong AckermanFunction(ulong m, ulong n)
+{
+    ulong result = 0;
+  if (m == 0) result = n+1;
+  else
+    if (m != 0 && n == 0)
+      result =  AckermanFunction (m - 1, 1);
+    else
+      result =  AckermanFunction (m - 1, AckermanFunction (m, n - 1));
+      return result;
+}
+ulong ackerman = AckermanFunction (Convert.ToUInt64(firstNonNegativeNumber), Convert.ToUInt64(secondNonNegativeNumber));
+Console.WriteLine($"Ответ:  {ackerman}");
+Console.WriteLine();
