21c9f6e22beffad19e76202c1fbe662a23650542
Sh-OV <shchegolga@yandex.ru>
2022-07-27 11:34:47 +0500

    Task_69 мое решение

--- Task_69/Program.cs
+++ Task_69/Program.cs
@@ -1,3 +1,38 @@
 ﻿/* Задача № 69. Напишите программу, которая на вход принимает два числа А и В,
                 и возводит число А в целую степень В с помощью рекурсии
                 А = 3; В = 5 -> 243(3^5)        */
+Console.Clear();
+Console.WriteLine("Для возведения чмисла А в степень В");
+Console.Write("Введите натуральное число А: ");
+int firstNaturalNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число В: ");
+int secondNaturalNumber = Convert.ToInt32(Console.ReadLine());
+
+while (firstNaturalNumber < 1)    // проверка
+{
+    Console.Write("Вы ввели неверные данные. Число А должно быть натуральным (от 1 и более): ");
+    firstNaturalNumber = Convert.ToInt32(Console.ReadLine());
+}
+
+while (secondNaturalNumber < 1)    // проверка
+{
+    Console.Write("Вы ввели неверные данные. Число В должно быть натуральным (от 1 и более): ");
+    secondNaturalNumber = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+int ExponentiationNumber(int a, int b)
+{
+    int i = 1;
+   if (b >= i)
+   {
+        i++;
+        return a * i + ExponentiationNumber(a, b-1);
+   }
+   else return 0;
+}
+int result = ExponentiationNumber(firstNaturalNumber, secondNaturalNumber);
+Console.WriteLine($"A = {firstNaturalNumber}; B = {secondNaturalNumber} -> {result} ({firstNaturalNumber}^{secondNaturalNumber})");
+Console.WriteLine();
+
